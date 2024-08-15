82df23b68fa10ceade897633463d4ed2deb956c4
wqyzstql <1139198820@qq.com>
2024-08-15 11:41:52 +0800

    fix bug of ex2

--- lab03/ex2.s
+++ lab03/ex2.s
@@ -23,9 +23,9 @@ dest:
 
 .text
 fun:
-    addi t0, a0, 1
-    sub t1, x0, a0
-    mul a0, t0, t1
+    addi t0, a0, 1 #a0 is the value of x
+    sub t1, x0, a0 #t1 = -x
+    mul a0, t0, t1 # a0=-x*(x+1)
     jr ra
 
 main:
@@ -35,29 +35,31 @@ main:
     sw s1, 4(sp)
     sw s2, 8(sp)
     sw s3, 12(sp)
-    sw ra, 16(sp)
+    sw ra, 16(sp) #store the address
     # END PROLOGUE
     addi t0, x0, 0
     addi s0, x0, 0
     la s1, source
     la s2, dest
 loop:
-    slli s3, t0, 2
+    slli s3, t0, 2 #s3 = t0<<2
     add t1, s1, s3
-    lw t2, 0(t1)
-    beq t2, x0, exit
-    add a0, x0, t2
-    addi sp, sp, -8
+    lw t2, 0(t1)#t2 = source[s3] t2=k
+    beq t2, x0, exit#if t2==0 break
+    add a0, x0, t2#a0 = t2
+
+    addi sp, sp, -8#call func
     sw t0, 0(sp)
     sw t2, 4(sp)
     jal fun
     lw t0, 0(sp)
     lw t2, 4(sp)
-    addi sp, sp, 8
-    add t2, x0, a0
-    add t3, s2, s3
+    addi sp, sp, 8#return func then a0 is the ans
+
+    add t2, x0, a0#t2 = a0
+    add t3, s2, s3#t3 = dest[s3]
     sw t2, 0(t3)
-    add s0, s0, t2
+    add s0, s0, t2#s0 = s0+dest[s3], s0=sum
     addi t0, t0, 1
     jal x0, loop
 exit:
@@ -70,4 +72,5 @@ exit:
     lw ra, 16(sp)
     addi sp, sp, 20
     # END EPILOGUE
-    jr ra
+    addi a0, x0, 10
+    ecall
